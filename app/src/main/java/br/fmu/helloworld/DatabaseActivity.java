254bbbef62d99e6a750426bf9bbb42d0668150b9
eanassu <eanassu@gmail.com>
2024-10-02 20:34:25 -0300

    Aula 02/10

--- app/src/main/java/br/fmu/helloworld/DatabaseActivity.java
+++ app/src/main/java/br/fmu/helloworld/DatabaseActivity.java
@@ -3,19 +3,97 @@ package br.fmu.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatabaseActivity extends AppCompatActivity {
 
+    private EditText editTextRe;
+    private EditText editTextNome;
+    private EditText editTextDataAdmissao;
+    private EditText editTextSalario;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        editTextRe = findViewById(R.id.editTextRe);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextDataAdmissao = findViewById(R.id.editTextDataAdmissao);
+        editTextSalario = findViewById(R.id.editTextSalario);
+    }
 
+    public void cadastrar(View view) {
         AppDatabase db = AppDatabase.getInstance(this);
         FuncionarioDao dao = db.funcionarioDao();
-        Funcionario f = new Funcionario(1,"Teste1",new Date(),1000);
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao,salario);
         dao.insert(f);
     }
+
+    public void limpar(View view) {
+        editTextRe.setText("");
+        editTextNome.setText("");
+        editTextDataAdmissao.setText("");
+        editTextSalario.setText("");
+    }
+
+    public void buscar(View view) {
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        Funcionario f = dao.buscarPeloRe(re);
+        editTextNome.setText(f.getNome());
+        editTextDataAdmissao.setText(dateFormat.format(f.getDataAdmissao()));
+        editTextSalario.setText(Double.toString(f.getSalario()));
+    }
+
+    public void excluir(View view) {
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao,salario);
+        dao.delete(f);
+    }
+
+    public void alterar(View view) {
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao,salario);
+        dao.update(f);
+    }
+
+
 }
